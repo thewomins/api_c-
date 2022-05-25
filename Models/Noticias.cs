d831bb3de1f0fee623d8923ca4ebf63b9df6de97
thewomins <thewomins@gmail.com>
2022-05-25 12:19:44 -0400

    fixed noticias

--- Models/Noticias.cs
+++ Models/Noticias.cs
@@ -3,13 +3,14 @@ public class Noticias{
     public int id {set;get;}
     public string titulo {set;get;} = String.Empty;
     public string descripcion {set;get;} = String.Empty;
-    public List<string> comentario {set;get;}
-}
+    public List<string> comentarios {set;get;} = null;
 
-public Noticias(int id, string titulo, string descripcion, list< string> comentarios)
-{
-    this.comentarios=comentarios;
-    this.id=id;
-    this.titulo=titulo;
-    this.descripcion=descripcion;
+    public Noticias(){
+    }
+    public Noticias(int id, string titulo, string descripcion, List<string> comentarios){
+        this.comentarios=comentarios;
+        this.id=id;
+        this.titulo=titulo;
+        this.descripcion=descripcion;
+    }
 }
